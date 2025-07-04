86a1ec31201ca7d95157356453bf3eda34a19143
Oğuzhan <gasmekegitim321@gmail.com>
2025-07-04 22:19:05 +0300

    Update main.ino

--- Kodlar/main.ino
+++ Kodlar/main.ino
@@ -1,39 +1,38 @@
-#include <deneyapkartkamera.h> // Deneyap Kamera tanımlanır.
-#define PIR_PIN 2  // PIR Hareket sensörü tanımlanır.
-                   // Bunları DENEYAP IDE içerisindeki kendi kütüphane indirme bölümündne indirebilirisniz.
+#include <deneyapkartkamera.h> // Deneyap Kamera modülü için gerekli kütüphane
 
-bool hareketVarMi = false; // Sonraki bildirimler için bir sıfır noktası.
-bool cisimVarMi = false;
+#define PIR_PIN 2  // PIR Hareket Sensörü D2 pinine bağlandı
+
+bool hareketVarMi = false; // PIR sensörü hareket algılarsa true olur
+bool cisimVarMi = false;   // Kamera bir cisim algılarsa true olur
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(PIR_PIN, INPUT);   
+  Serial.begin(9600);       // Seri haberleşme başlatıldı
+  pinMode(PIR_PIN, INPUT);  // PIR pini giriş olarak ayarlandı
 
-  if (!DeneyapCamera.begin()) {
+  if (!DeneyapCamera.begin()) {  // Kamera başlatılamazsa hata mesajı ver
     Serial.println("Kamera başlatılamadı"); 
-    while (1);
+    while (1); // Sürekli döngüye gir, çünkü kamera çalışmıyor
   }
 }
 
 void loop() {
-  hareketVarMi = digitalRead(PIR_PIN);  
+  hareketVarMi = digitalRead(PIR_PIN); // PIR sensöründen okuma yapılır
 
-  
-  if (DeneyapCamera.detectObject()) {  // Kamera cisimleri algılamaya başlıyor. 
+  if (DeneyapCamera.detectObject()) {  // Kamera cisim algılarsa
     cisimVarMi = true;
   } else {
     cisimVarMi = false;
   }
 
-  
   if (cisimVarMi) {
     if (hareketVarMi) {
-      Serial.println(" Canlı Biri Var Ve Hareket Ediyor.");  // PIR sensörü hareketi algılamaya başlıyor.
+      Serial.println("Canlı biri var ve hareket ediyor."); // Cisim var + hareket var
     } else {
-      Serial.println(" Canlı Var Ama Hareket Etmiyor");
+      Serial.println("Canlı var ama hareket etmiyor.");    // Cisim var + hareket yok
     }
   } else {
-    Serial.println("Canlı Göremedim");
+    Serial.println("Canlı göremedim.");  // Kamera hiçbir şey algılamadı
   }
 
-  delay(1000);       // İşlem arası 1 sn  bir duraksama. 
+  delay(1000); // 1 saniye bekle
+}
